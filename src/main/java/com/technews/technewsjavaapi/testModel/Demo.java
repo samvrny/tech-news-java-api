5d7393318d8455a35eb9f665033102405ec42dac
samvrny <samvrny@gmail.com>
2023-03-31 16:24:39 -0500

    did some demo testing

--- src/main/java/com/technews/technewsjavaapi/testModel/Demo.java
+++ src/main/java/com/technews/technewsjavaapi/testModel/Demo.java
@@ -1,13 +1,16 @@
 package com.technews.technewsjavaapi.testModel;
 
+import java.util.Objects;
+
 public class Demo {
+
+    private String name;
+    private int age;
+
     public Demo(String name, int age) {
         this.name = name;
         this.age = age;
     }
-
-    private String name;
-    private int age;
 
     public String getName() {
         return name;
@@ -23,5 +26,26 @@ public class Demo {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Demo demo = (Demo) o;
+        return age == demo.age && Objects.equals(name, demo.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Demo{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
